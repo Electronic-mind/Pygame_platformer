e81aad051f05a3d830fd7de3ebf695bff6384eaf
Judi | Programmer <75368942+Electronic-mind@users.noreply.github.com>
2022-09-23 16:54:55 +0300

    .

--- Demo/New_game.py
+++ Demo/New_game.py
@@ -9,12 +9,12 @@ WINDOW_SIZE = [600, 400]
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
-display = pygame.Surface((300, 200))
+#display = pygame.Surface((300, 200))
 
-player_image = pygame.image.load("Saudi_lady-32x10.png")
+player_image = pygame.image.load("Saudi_lady-32bit.png")
 
-grass_image = pygame.image.load("Grass.png")
-dirt_image = pygame.image.load("dirt .png")
+grass_image = pygame.image.load("Grass_new.png")
+dirt_image = pygame.image.load("Dirt_new.png")
 TILE_SIZE = grass_image.get_width()
 
 game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
@@ -75,16 +75,16 @@ player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_hei
 air_timer = 0
 
 while True:
-    display.fill((146, 244, 255))
+    screen.fill((146, 244, 255))
     tile_rects = []
     y = 0
     for row in game_map:
         x = 0
         for tile in row:
             if tile == '1':
-                display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+                screen.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
             if tile == '2':
-                display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+                screen.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
             if tile != '0':
                 tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
             x += 1
@@ -110,7 +110,7 @@ while True:
     if collisions['top']:
         player_y_momentum += 2
 
-    display.blit(player_image, [player_rect.x, player_rect.y])
+    screen.blit(player_image, [player_rect.x, player_rect.y])
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -131,7 +131,7 @@ while True:
             if event.key == K_RIGHT:
                 moving_r = False
 
-    surf = pygame.transform.scale(display, WINDOW_SIZE)
-    screen.blit(surf, (0, 0))
+    #surf = pygame.transform.scale(display, WINDOW_SIZE)
+    #screen.blit(surf, (0, 0))
     pygame.display.update()
     clock.tick(60)
