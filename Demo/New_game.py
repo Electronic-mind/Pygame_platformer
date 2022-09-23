9acc586a4eacd3078154a6f3bc38620f4c77a92e
Judi | Programmer <75368942+Electronic-mind@users.noreply.github.com>
2022-09-23 17:35:36 +0300

    separated the map in a different text file

--- Demo/New_game.py
+++ Demo/New_game.py
@@ -8,30 +8,40 @@ clock = pygame.time.Clock()
 WINDOW_SIZE = [600, 400]
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+pygame.display.set_caption("Happy national day!")
 
 #display = pygame.Surface((300, 200))
 
+
+
+
+#loading the map from game_map.txt
+def load_map(path):
+    f = open(path, 'r')
+    data = f.read()
+    f.close
+    data = data.split('\n')
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+
+
+game_map = load_map("game_map.txt")
+
+
+#load the image of the player
 player_image = pygame.image.load("Saudi_lady-32bit.png")
 
+#load the images of the tiles
 grass_image = pygame.image.load("Grass_new.png")
 dirt_image = pygame.image.load("Dirt_new.png")
 TILE_SIZE = grass_image.get_width()
 
-game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['2', '2', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2', '2'],
-            ['1', '1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
 
 
+
+#checking for collisions
 def collision_test(rects, tiles):
     hit_list = []
     for tile in tiles:
@@ -74,17 +84,29 @@ player_y_momentum = 0
 player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
 air_timer = 0
 
+#scrolling the screen
+true_scroll = [0,0]
+scroll = true_scroll.copy()
+scroll[0] = true_scroll[0]
+scroll[1] = true_scroll[1]
+
+
+
 while True:
-    screen.fill((146, 244, 255))
+
+    scroll[0] += (player_rect.x - scroll[0] - 284)/ 20
+    scroll[1] += (player_rect.y - scroll[1] - 200)/ 20
+
+    screen.fill((135, 206, 235))
     tile_rects = []
     y = 0
     for row in game_map:
         x = 0
         for tile in row:
             if tile == '1':
-                screen.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+                screen.blit(dirt_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
             if tile == '2':
-                screen.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+                screen.blit(grass_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
             if tile != '0':
                 tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
             x += 1
@@ -92,9 +114,9 @@ while True:
 
     player_movement = [0, 0]
     if moving_r:
-        player_movement[0] += 2
+        player_movement[0] += 5
     if moving_l:
-        player_movement[0] -= 2
+        player_movement[0] -= 5
     player_movement[1] += player_y_momentum
     player_y_momentum += 0.2
     if player_y_momentum > 3:
@@ -110,7 +132,7 @@ while True:
     if collisions['top']:
         player_y_momentum += 2
 
-    screen.blit(player_image, [player_rect.x, player_rect.y])
+    screen.blit(player_image, [player_rect.x - scroll[0], player_rect.y - scroll[1]])
 
     for event in pygame.event.get():
         if event.type == QUIT:
